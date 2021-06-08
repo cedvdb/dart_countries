1aab815efcd6999253db282cd3eb6caee0337159
cedvdb <cedvandenbosch@gmail.com>
2021-06-08 16:09:24 +0200

    example for pubpoints

---
+++ example/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dart_countries/src/dart_countries.dart';
+
+void main(List<String> arguments) {
+// creation
+  final allCountries = countries;
+  final us = Country.fromIsoCode('us');
+  print(us.name);
+  print(us.nativeName);
+  print(us.continent);
+  print(us.capital);
+  print(us.currencyCode);
+  print(us.dialCode);
+  print(us.displayDialCode);
+  print(us.flag);
+  print(us.languages);
+}
